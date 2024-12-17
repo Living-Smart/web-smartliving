0768612294c983c6657517d89da20d424e0904d0
hara29 <cindymaharani29@gmail.com>
2024-12-17 17:38:48 +0700

    menambah navigasi ke artikel (blog.html)

---
+++ template/content.php
@@ -0,0 +1,234 @@
+<div class="views">
+      <div class="view view-main">
+        <div class="pages">
+          <div data-page="index" class="page homepage">
+            <div class="page-content">
+              <div class="navbarpages">
+                <div class="navbar_left">
+                  <div class="logo_text"><a href="index.html">Smart Living</a></div>
+                </div>
+                <div class="navbar_right navbar_right_menu">
+                  <a href="#" data-panel="left" class="open-panel"
+                    ><img src="images/icons/white/menu.png" alt="" title=""
+                  /></a>
+                </div>
+                <div class="navbar_right" id="loginButton" style="display:none;">
+                  <a href="#" data-popup=".popup-login" class="open-popup"><button id="showLoginPopup">Login</button></a>
+                </div>
+                <div class="navbar_right" id="userButton">
+                  <a href="#" data-panel="right" class="open-panel"
+                    ><img src="images/icons/white/user.png" alt="" title=""
+                  /></a>
+                </div>
+              </div>
+
+              <!-- Slider -->
+              <div
+                class="swiper-container slidertoolbar swiper-init"
+                data-effect="slide"
+                data-parallax="true"
+                data-pagination=".swiper-pagination"
+                data-next-button=".swiper-button-next"
+                data-prev-button=".swiper-button-prev"
+              >
+                <div class="swiper-wrapper">
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/jamalislide1.jpg)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <!-- <span class="subtitle" data-swiper-parallax="-60%"
+                          >CREATIVE SOLUTION</span
+                        > -->
+                        <h2 data-swiper-parallax="-100%">
+                          JAMALI SMART LIVING
+                        </h2>
+
+                        <p data-swiper-parallax="-30%">
+                          Mewujudkan literasi di bidang kesehatan
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          dalam rangka pencegahan kejadian stunting dan pernikahan dini 
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          dan pemantauan-penyampaian informasi pemeliharaan kesehatan remaja 
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          serta peningkatan kesadaran keselamatan berkendara
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/remajadananemnia.jpeg)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <span class="subtitle" data-swiper-parallax="-60%">NUTRISI REMAJA</span>
+                        <span class="subtitle" data-swiper-parallax="-60%">PECEGAHAN ANEMIA</span>
+                        <h2 data-swiper-parallax="-100%">PENYULUHAN REMAJA DAN ANEMIA</h2>
+
+                        <p data-swiper-parallax="-30%">
+                          Penyuluhan Nutrisi Remaja dan Pencegahan Anemia di SMPN 3 Mande
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          dengan luaran buku saku 'Remaja dan Anemia'
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/penyuluhanpernikahandini.jpeg)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <span class="subtitle" data-swiper-parallax="-60%"
+                          >PENCEGAHAN PERNIKAHAN DINI</span
+                        >
+                        <h2 data-swiper-parallax="-100%">PENYULUHAN PENCEGAHAN PERNIKAHAN DINI</h2>
+
+                        <p data-swiper-parallax="-30%">
+                          Penyuluhan berisi tentang pengetahuan tentang kesehatan reproduksi, risiko ketika terjadi pernikahan dini dan dampak pernikahan dini bagi remaja di SMPN 3 Mande
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          dengan luaran buku saku 'Pencegahan Pernikahan Dini'
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/workshopsabun.png)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <span class="subtitle" data-swiper-parallax="-60%"
+                          >WORKSHOP</span
+                        >
+                        <h2 data-swiper-parallax="-100%">WORKSHOP PEMBUATAN SABUN ORGANIK</h2>
+
+                        <p data-swiper-parallax="-30%">
+                          Workshop pembuatan sabun organik yang bertujuan untuk meningkatkan keterampilan siswa
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          dengan luaran modul 'Pembuatan sabun organik'
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/penyuluhansafetyriding.jpeg)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <span class="subtitle" data-swiper-parallax="-60%"
+                          >ZoSS</span
+                        >
+                        <h2 data-swiper-parallax="-100%">SOSIALISASI SAFETY RIDING DAN ZOSS</h2>
+
+                        <p data-swiper-parallax="-30%">
+                          Sosialisasi Safety Riding dan Zona Selamat Sekolah (ZOSS)
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          untuk meningkatkan keselamatan berkendara di SMPN 3 Mande
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div
+                    class="swiper-slide"
+                    style="background-image: url(images/jamali/sahabatremaja.jpeg)"
+                  >
+                    <div class="slider_trans">
+                      <div class="slider-caption">
+                        <span class="subtitle" data-swiper-parallax="-60%"
+                          >SAHABAT REMAJA</span
+                        >
+                        <h2 data-swiper-parallax="-100%">PENCEGAAN HAMIL USIA MUDA DAN PERGAULAN BEBAS</h2>
+
+                        <p data-swiper-parallax="-30%">
+                          FGD (Forum Group Discussion) pencegahan Kehamilan Usia Muda,
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          Menghindari Pergaulan Bebas,
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          Pembentukan teman sebaya, dan
+                        </p>
+                        <p data-swiper-parallax="-30%">
+                          Simulasi Peran Teman Sebaya
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="swiper-pagination"></div>
+                <div class="swiper-button-prev"></div>
+                <div class="swiper-button-next"></div>
+              </div>
+
+              <div
+                class="swiper-container-toolbar swiper-toolbar swiper-init"
+                data-effect="slide"
+                data-slides-per-view="1"
+                data-slides-per-group="1"
+                data-space-between="0"
+                data-pagination=".swiper-pagination-toolbar"
+              >
+                <div class="swiper-pagination-toolbar"></div>
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide toolbar-icon">
+                    <a href="blog.html" data-view=".view-main"
+                      ><img
+                        src="images/icons/blue/Article.svg"
+                        alt=""
+                        title=""
+                      /><span>ARTIKEL</span>
+                    </a>
+                    <a href="features.html" data-view=".view-main"
+                      ><img
+                        src="images/icons/blue/Modul.svg"
+                        alt=""
+                        title=""
+                      /><span>BUKU SAKU REMAJA DAN ANEMIA</span>
+                    </a>
+                    <a href="#" data-popup=".popup-login" class="open-popup"
+                      ><img
+                        src="images/icons/blue/Modul.svg"
+                        alt=""
+                        title=""
+                      /><span>BUKU SAKU PENCEGAHAN PERNIKAHAN DINI</span>
+                    </a>
+                    <a href="blog.html" data-view=".view-main"
+                      ><img
+                        src="images/icons/blue/Modul.svg"
+                        alt=""
+                        title=""
+                      /><span>MODUL PEMBUATAN SABUN ORGANIK </span>
+                    </a>
+                    <a href="contact.html" data-view=".view-main"
+                      ><img
+                        src="images/icons/blue/Modul.svg"
+                        alt=""
+                        title=""
+                      /><span>BUKU SAKU RESIKO KEHAMILAN USIA MUDA</span>
+                    </a>
+                    <a href="photos.html" data-view=".view-main"
+                      ><img
+                        src="images/icons/blue/Modul.svg"
+                        alt=""
+                        title=""
+                      /><span>MODUL SAFETY RIDING DAN ZOSS</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
